42c41897bae4cbfee5ac8c748dc6ba6f541fca40
ucikdika7 <ucikdikamaharani@student.uns.ac.id>
2026-05-09 19:33:09 +0700

    up

--- api/logout.php
+++ api/logout.php
@@ -1,12 +1,6 @@
 <?php
 session_start();
-session_unset();
 session_destroy();
-
-// Pastikan file tujuan 'index.php' ada di folder yang sama
-echo "<script>
-    alert('Anda telah berhasil keluar.');
-    window.location='index.html'; 
-</script>";
-exit;
+header('Location: login.php');
+exit();
 ?>
