92d4e4ae481f3c17eeaa7c5f25f4ff6918ea023b
ucikdika7 <ucikdikamaharani@student.uns.ac.id>
2026-05-10 21:08:07 +0700

    up

--- api/koneksi.php
+++ api/koneksi.php
@@ -1,30 +1,15 @@
 <?php
-// Data dari TiDB Cloud
-$host = 'gateway01.ap-southeast-1.prod.alicloud.tidbcloud.com';
-$port = 4000;
-$user = '2tfzdzpXzpZ8yRy.root';
-$pass = 'xG0M9RxORnl8ZUZz';
-$db   = 'monitoring_tiket2';
+$host     = "gateway01.ap-southeast-1.prod.alicloud.tidbcloud.com";
+$port     = 4000;
+$username = "2tfzdzpXzpZ8yRy.root";
+$password = "xG0M9RxORnl8ZUZz";
+$database = "monitoring_tiket2";
 
-// Inisialisasi mysqli
-$koneksi = mysqli_init();
+$koneksi = mysqli_connect($host, $username, $password, $database, $port);
 
-// Menambahkan pengaturan SSL (Wajib untuk TiDB Serverless)
 mysqli_ssl_set($koneksi, NULL, NULL, NULL, NULL, NULL);
 
-// Melakukan koneksi
-$real_connect = mysqli_real_connect(
-    $koneksi, 
-    $host, 
-    $user, 
-    $pass, 
-    $db, 
-    $port, 
-    NULL, 
-    MYSQLI_CLIENT_SSL
-);
-
-if (!$real_connect) {
-    die("Koneksi ke TiDB Cloud gagal: " . mysqli_connect_error());
+if (!$koneksi) {
+    die("Koneksi gagal: " . mysqli_connect_error());
 }
 ?>
