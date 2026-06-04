281720fc8f417a2d00c61c39c591061f06e2086a
ucikdika7 <ucikdikamaharani@student.uns.ac.id>
2026-06-04 13:34:17 +0700

    up

--- api/logout.php
+++ api/logout.php
@@ -1,13 +1,6 @@
 <?php
 session_start();
-session_unset();
 session_destroy();
-
-// Hapus cookie login
-setcookie('user_id', '', time() - 3600, "/");
-setcookie('user_nama', '', time() - 3600, "/");
-setcookie('user_role', '', time() - 3600, "/");
-
 header('Location: /api/login.php');
 exit();
 ?>
